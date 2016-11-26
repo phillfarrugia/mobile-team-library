81a14a1c56464982c22d0e8bb5376db1e40aba13
Phill Farrugia <me@phillfarrugia.com>
2016-11-26 16:05:47 +1100

    Adds a Book Cell and View Model

---
+++ Prolific Library/Cells/BookTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  BookTableViewCell.swift
+//  Prolific Library
+//
+//  Created by Phill Farrugia on 26/11/16.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+import UIKit
+
+class BookTableViewCell: UITableViewCell {
+    
+    @IBOutlet private var titleLabel: UILabel!
+    @IBOutlet private var authorLabel: UILabel!
+    
+}
---
+++ Prolific Library/View Models/BookCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  BookCellViewModel.swift
+//  Prolific Library
+//
+//  Created by Phill Farrugia on 26/11/16.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+import Foundation
+
+struct BookCellViewModel {
+    
+}
