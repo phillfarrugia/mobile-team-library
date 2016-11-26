828d74dc5afc01423c113cbab91556e0e4348cc1
Phill Farrugia <me@phillfarrugia.com>
2016-11-26 16:00:10 +1100

    Adds an empty Table View

--- Prolific Library/View Controllers/BookListViewController.swift
+++ Prolific Library/View Controllers/BookListViewController.swift
@@ -9,11 +9,13 @@
 import UIKit
 
 class BookListViewController: UIViewController {
+    
+    @IBOutlet private var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        title = "Books"
     }
 
 }
