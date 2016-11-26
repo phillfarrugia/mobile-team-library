1c1b0dfb2c076225399f381d23575767d94cbb7c
Phill Farrugia <me@phillfarrugia.com>
2016-11-26 14:12:34 +1100

    Adds new Xcode project

---
+++ Prolific Library/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Prolific Library
+//
+//  Created by Phill Farrugia on 26/11/16.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Prolific Library/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Prolific Library
+//
+//  Created by Phill Farrugia on 26/11/16.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
