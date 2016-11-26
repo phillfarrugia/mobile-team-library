e1b5ee88b243027050a4a4f76b7d651d0f85c851
Phill Farrugia <me@phillfarrugia.com>
2016-11-26 16:46:23 +1100

    Adds an AddBookViewController

---
+++ Prolific Library/View Controllers/AddBookViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddBookViewController.h
+//  Prolific Library
+//
+//  Created by Phill Farrugia on 26/11/16.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddBookViewController : UIViewController
+
+@end
---
+++ Prolific Library/View Controllers/AddBookViewController.m
@@ -0,0 +1,22 @@
+//
+//  AddBookViewController.m
+//  Prolific Library
+//
+//  Created by Phill Farrugia on 26/11/16.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+#import "AddBookViewController.h"
+
+@interface AddBookViewController ()
+
+@end
+
+@implementation AddBookViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+@end
