365107f526bcda31aab3a23f10acdc527f7bb3d5
Phill Farrugia <me@phillfarrugia.com>
2016-11-26 15:30:24 +1100

    Adds Bridging Header and Swift View Controller

---
+++ Prolific Library/View Controllers/BookListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BookListViewController.swift
+//  Prolific Library
+//
+//  Created by Phill Farrugia on 26/11/16.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+import UIKit
+
+class BookListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+}
