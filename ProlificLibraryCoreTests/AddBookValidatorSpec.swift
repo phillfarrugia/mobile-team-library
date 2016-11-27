66eae031b7dcd3f7a9577e0631374375934da074
Phill Farrugia <me@phillfarrugia.com>
2016-11-27 13:20:38 +1100

    Implements expectations for book validator

--- ProlificLibraryCoreTests/AddBookValidatorSpec.swift
+++ ProlificLibraryCoreTests/AddBookValidatorSpec.swift
@@ -16,37 +16,37 @@ class AddBookValidatorSpec: QuickSpec {
             
             context("all empty", {
                 it("should return required fields incompete", closure: {
-                    
+                    expect(AddBookValidator.validate(titleText: "", authorText: "", publisherText: "", categoriesText: "")).to(equal(AddBookValidationState.RequiredFieldsIncomplete))
                 })
             })
             
             context("title", {
                 it("should return required fields incompete", closure: {
-                    
+                    expect(AddBookValidator.validate(titleText: "random book title", authorText: "", publisherText: "", categoriesText: "")).to(equal(AddBookValidationState.RequiredFieldsIncomplete))
                 })
             })
             
             context("title and author", {
                 it("should return incompete", closure: {
-                    
+                    expect(AddBookValidator.validate(titleText: "book name", authorText: "author name here", publisherText: "", categoriesText: "")).to(equal(AddBookValidationState.Incomplete))
                 })
             })
             
             context("title, author and publisher", {
                 it("should return incompete", closure: {
-                    
+                    expect(AddBookValidator.validate(titleText: "book name", authorText: "author name here", publisherText: "publishing house", categoriesText: "")).to(equal(AddBookValidationState.Incomplete))
                 })
             })
             
             context("title, author, publisher and categories", {
-                it("should return incompete", closure: {
-                    
+                it("should return complete", closure: {
+                    expect(AddBookValidator.validate(titleText: "book name", authorText: "author name here", publisherText: "publishing house", categoriesText: "ios, dev, books")).to(equal(AddBookValidationState.Complete))
                 })
             })
             
             context("no title", {
                 it("should return required fields incompete", closure: {
-                    
+                    expect(AddBookValidator.validate(titleText: "", authorText: "erica sadun", publisherText: "objcio", categoriesText: "ios, swift, programming")).to(equal(AddBookValidationState.RequiredFieldsIncomplete))
                 })
             })
             
