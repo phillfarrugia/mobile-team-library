4e70cb2f72c5d5fd367416b19033d313cfeed981
Phill Farrugia <me@phillfarrugia.com>
2016-11-27 13:11:31 +1100

    Adds an empty AddBookValidatorSpec

---
+++ ProlificLibraryCoreTests/AddBookValidatorSpec.swift
@@ -0,0 +1,16 @@
+//
+//  ProlificLibraryCoreTests.swift
+//  ProlificLibraryCoreTests
+//
+//  Created by Phill Farrugia on 27/11/16.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+import Quick
+import Nimble
+
+class AddBookValidatorSpec: QuickSpec {
+    override func spec() {
+
+    }
+}
