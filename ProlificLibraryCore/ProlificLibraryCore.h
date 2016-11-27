80138d0df3d4ee27ef3a345009d91b6b0cf7a06b
Phill Farrugia <me@phillfarrugia.com>
2016-11-27 12:32:05 +1100

    Adds ProlificLibraryCore framework target

---
+++ ProlificLibraryCore/ProlificLibraryCore.h
@@ -0,0 +1,19 @@
+//
+//  ProlificLibraryCore.h
+//  ProlificLibraryCore
+//
+//  Created by Phill Farrugia on 27/11/16.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ProlificLibraryCore.
+FOUNDATION_EXPORT double ProlificLibraryCoreVersionNumber;
+
+//! Project version string for ProlificLibraryCore.
+FOUNDATION_EXPORT const unsigned char ProlificLibraryCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ProlificLibraryCore/PublicHeader.h>
+
+
