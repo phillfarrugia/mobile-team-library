2efc6519ae4f606b72884d30e94852427b40ead3
Phill Farrugia <me@phillfarrugia.com>
2016-11-28 21:55:20 +1100

    Tweaks vertical constraints on book table view cell

---
+++ Prolific Library/View Controllers/TagsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TagsViewController.swift
+//  Prolific Library
+//
+//  Created by Phill Farrugia on 28/11/16.
+//  Copyright © 2016 Phill Farrugia. All rights reserved.
+//
+
+import Foundation
+
+class TagsViewController: UIViewController {
+    
+}
